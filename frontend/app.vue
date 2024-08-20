d9d44a4e5efdc0fa3c8b046697aadc0cb329b3b2
aaronthom <hampeaaron@gmail.com>
2024-08-20 23:03:46 +0200

    finished header

--- frontend/app.vue
+++ frontend/app.vue
@@ -7,5 +7,7 @@
 </template>
 
 <style>
-
+* {
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
+}
 </style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,48 @@
-
 <template>
-    <h1>TRISIGHT</h1>
-</template>
-
+    <div class="home">
+      <header>
+        <img src="/images/TRISIGHT-LOGO-TRANSPARENT-NEU.png" alt="TRISIGHT Logo" class="logo"/>
+        <button class="login-button">Login</button>
+      </header>
+    </div>
+  </template>
+  
+  <style scoped>
+  .home {
+    background-color: #185262; 
+    color: white; 
+    min-height: 100vh; 
+  }
+  
+  header {
+    display: flex;  
+    justify-content: space-between; 
+    align-items: center; 
+    height: 100px;
+    padding: 0 20px; 
+  }
+  
+  .logo {
+    height: 60px;
+  }
+  
+  .login-button {
+    background-color: white; 
+    width: 140px;
+    color: #185262; 
+    border: none; 
+    padding: 10px 20px;
+    margin-right: 30px; 
+    border-radius: 20px; 
+    cursor: pointer; 
+    font-size: 16px; 
+    font-weight: bold; 
+    transition: background-color 0.3s, color 0.3s;
+  }
+  
+  .login-button:hover {
+    background-color: #0f3e4b; 
+    color: white;
+  }
+  </style>
+  
