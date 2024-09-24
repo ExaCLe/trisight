1365812f0213fc809d90d7c62954a3de9bebdd44
Leon Biermann <biermann.leon@outlook.de>
2024-09-24 19:43:14 +0200

    Add runtime configuration for backend URL and update playscreen to use it

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   pages: true,
-  modules: ["@nuxt/ui"]
+  modules: ["@nuxt/ui"], 
+  runtimeConfig: {
+    public: {
+      backendUrl: process.env.BACKEND_URL || "http://localhost:8000",
+    }
+  }
 })
