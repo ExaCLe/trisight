792400f0b1188d2fa69324a25f92375f59009451
Leon Biermann <biermann.leon@outlook.de>
2024-10-09 18:09:13 +0200

    fix config

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./test.db"  # Default to SQLite for development
-    testing: bool = False
 
     class ConfigDict:
         env_file = ".env"  # Load environment variables from a .env file
